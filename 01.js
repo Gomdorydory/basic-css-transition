4ed5ca33ed84386ec5bf1b6b125e43879a275639
gomdorydory <aldzlaldzl88@naver.com>
2022-05-18 21:38:04 +0900

    first commit

---
+++ 01.js
@@ -0,0 +1,10 @@
+const startBtn = document.querySelector('#startBt');
+
+startBtn.addEventListener('click', () => {
+  document.getElementById('start').style.display="none";
+  document.getElementById('quiz').style.display="block";  
+});
+
+startBtn.addEventListener('mousedown', () => {
+
+});
